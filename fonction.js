eceb62bc4e0d690cb764fccff0ed54b60c60cd23
Sethios Momentum <sethiosmomentum@gmail.com>
2025-01-18 19:52:10 +0200

    fonction fleche

---
+++ fonction.js
@@ -0,0 +1,23 @@
+// les fonctions declarative
+
+function sayHelo(){
+    console.log("Bonjour !")
+}
+
+sayHelo()
+
+// Expression de fonction
+
+let sayHello = function (){
+    console.log(" Hello brother !")
+}
+
+sayHello()
+
+// les fonction fleche
+
+let direBonjour = ()=>{
+    console.log("Fonction Fleche !")
+}
+
+direBonjour() 
