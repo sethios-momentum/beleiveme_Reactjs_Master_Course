ad1c7d48d06c1f72c0672224a39379cce80c6f22
Sethios Momentum <sethiosmomentum@gmail.com>
2025-01-18 15:46:06 +0200

    problem de var/ solve by let

---
+++ test.js
@@ -0,0 +1,14 @@
+var variablevar = "global"
+let variablelet = "global"
+
+
+if (true) {
+    var variablevar = "local"
+let variablelet = "local"
+
+console.log("var : "+ variablevar)
+console.log('let : '+variablelet) 
+}
+
+console.log("var : "+ variablevar)
+console.log('let : '+variablelet) 
